50ada78f08638f8c1f0ee85e71e7403407a0b4bb
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-01-22 11:26:28 +0530

    4 done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include"color.h"
+#include"ray.h"
 #include"vec3.h"
 #include<iostream>
 
 using namespace std;
+color ray_color(const ray& r){
+    return color(0,0,0);
+}
 int main(){
     int imgw,imgh;
     imgw = 256;
     imgh = 256;
+    auto aspectRatio = 16.0/9.0;
+    imgw = 400;
+    imgh = static_cast<int>(imgw/aspectRatio);
+    imgh = imgh<1 ? 1: imgh;
+    auto viewportHeight = 2.0;
+    auto viewportWidth = viewportHeight*(static_cast<double>(imgw)/imgh);
+    point3 camera(0,0,0);
     cout<<"P3\n"<<imgw<<' '<<imgh<<"\n255\n";
     for(int j=0;j<imgh;j++){
         clog<<"\rScanlines remaining: "<<(imgh-j)<<" "<<flush;
@@ -20,4 +31,5 @@ int main(){
         }
     }
     clog<<"\rDone                   \n";
+    
 }
